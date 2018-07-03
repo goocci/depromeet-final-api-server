7e9700c6a7c07122bbceb1980b80007baae34b7b
mingoo-kim <kimmingoo73@gmail.com>
2018-07-04 00:39:00 +0900

    [Update] Project Schema

--- models/project.js
+++ models/project.js
@@ -4,19 +4,27 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Project = new Schema({
+  userId: String, // 사용자 고유 id(=작성자)
   title: String, // 제목
   text: String, // 본문
+  startDt: Date, // 프로젝트 시작일시
+  endDt: Date, // 프로젝트 끝일시
   skillCode: { // 필요 기술코드 목록
-    designer: [String], // 디자인
-    developer: [String] // 개발자
+    designer: [String], // 디자이너 기술코드 목록
+    developer: [String] // 개발자 기술코드 목록
   },
   attachments: [{ // 첨부파일 목록
     fileName: String, // 파일명
     s3Location: String, // AWS S3 경로
     size: Number // 파일 사이즈
   }],
-  created_dt: {type: Date, default: Date.now}, // 생성 일시
-  updated_dt: {type: Date, default: Date.now}, // 수정 일시
+  hits: Number, // 조회수
+  applicant: [{ // 지원자 목록
+    userId: String, // 지원자 고유 id
+    join: Boolean, // 참여 여부
+  }],
+  createdDt: {type: Date, default: Date.now}, // 생성 일시
+  updatedDt: {type: Date, default: Date.now}, // 수정 일시
 },
 {collection: 'project'}
 )
