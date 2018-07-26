1a72300dfcea5ca8750f934bb3c61597fff9828d
mingoo-kim <kimmingoo73@gmail.com>
2018-07-26 21:54:32 +0900

    [Update] Get My Profile Tooltip API

--- api/user/index.js
+++ api/user/index.js
@@ -8,5 +8,6 @@ const profile = require('./profile')
 router.post('/login', login.socialLogin) // 소셜로그인 (회원가입)
 router.post('/profileWrite', profile.write) // 프로필 작성
 router.get('/myProfile', profile.getMyProfile) // 나의 프로필 조회
+router.get('/myProfile/tooltip', profile.getMyProfileTooltip) // 나의 프로필 Tooltip 조회
 
 module.exports = router
