8200a12295d24f97998d38442bed49df86e35e20
mingoo-kim <kimmingoo73@gmail.com>
2018-08-03 21:22:40 +0900

    [Update] Check Notice API

---
+++ api/notice/check.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const User = require('../../models/user')
+const Notice = require('../../models/notice')
+
+/**
+ * [API] 알림 확인
+ * @param {*} req
+ * @param {*} res
+ */
+exports.checkNotice = (req, res) => {
+  const noticeId = req.body.noticeId
+
+  // 0. 요청 바디 확인
+  const checkReqBody = () => {
+    return new Promise((resolve, reject) => {
+      if (!noticeId) {
+        return reject({
+          code: 'request_body_error',
+          message: 'request body is not defined'
+        })
+      } else resolve()
+    })
+  }
+
+  // 1. 알림 확인
+  const check = () => {
+    return Notice
+          .findOneAndUpdate({
+            _id: noticeId
+          }, {
+            $set: {
+              isCheck: true,
+              updatedDt: new Date()
+            }
+          })
+  }
+
+  // 2. 응답
+  const resp = () => {
+    res.status(200).json({
+      code: 'suc_check_notice',
+      message: '알림 확인 성공'
+    })
+  }
+
+  checkReqBody()
+  .then(check)
+  .then(resp)
+  .catch((err) => {
+    console.error(err)
+    return res.status(500).json(err.message || err)
+  })
+}
